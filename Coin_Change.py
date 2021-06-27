4bdf5077b42184630997a6d8d888e69f5b3c30bb
yashwant94308 <yashwant94308@gmail.com>
2021-06-27 14:34:52 +0530

    "DSA450"

--- Coin_Change.py
+++ Coin_Change.py
@@ -1,21 +1,38 @@
-coins = [1, 3, 4, 5]
-amount = 7
-res = [1e9]
+coins = [2,3,4,5]
+
+amount = 130050
+dp = [1e9] * (amount + 1)
+dp[0] = 0
+
+# dp
+def dpD():
+    for i in range(amount + 1):
+        for c in coins:
+            if i-c >= 0:
+                dp[i] = min(dp[i], 1+dp[i-c])
 
 
-def dfs(arr, n, t, cnt):
-    if t == 0:
-        res[0] = min(res[0], cnt)
-        return
-    if t < 0 or n < 0:
-        return
-    cnt += 1
-    # print(cnt)
-    dfs(arr, n, t - arr[n], cnt)
-    cnt -= 1
-    # print(cnt)
-    dfs(arr, n - 1, t, cnt)
+dpD()
+# print(dp)
+if dp[amount] == 1e9 or dp[amount] == 1e9 - 1:
+    print(-1)
+else:
+    print(dp[amount])
 
-
-dfs(coins, len(coins) - 1, amount, 0)
-print(res[0])
+# recursive
+# def dfs(arr, n, t, cnt):
+#     if t == 0:
+#         res[0] = min(res[0], cnt)
+#         return
+#     if t < 0 or n < 0:
+#         return
+#     cnt += 1
+#     # print(cnt)
+#     dfs(arr, n, t - arr[n], cnt)
+#     cnt -= 1
+#     # print(cnt)
+#     dfs(arr, n - 1, t, cnt)
+#
+#
+# dfs(coins, len(coins) - 1, amount, 0)
+# print(res[0])
